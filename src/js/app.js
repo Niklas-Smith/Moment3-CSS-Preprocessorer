246ab4d1691fa045811e8d33371a50a1e0d4dc06
Niklas Smith <niklas_smith@hotmail.com>
2025-04-28 14:51:10 +0200

    Made Nav Meny

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,27 @@
+"use strict"
 
+
+let openBtn = document.getElementById("open-menu");
+let closeBtn = document.getElementById("close-menu");
+
+
+openBtn.addEventListener('click', toggleMenu);
+closeBtn.addEventListener('click', toggleMenu);
+
+
+
+function toggleMenu() {
+    let navMenuEl = document.getElementById("nav-menu");
+
+   
+    let style = window.getComputedStyle(navMenuEl);
+
+    if(style.display === "none") {
+        navMenuEl.style.display = "block";
+
+    } else {
+        navMenuEl.style.display = "none";
+    }
+
+}    
+// much of my code for burgermeny and meny is taken from Malin larsson(MallarMiun) (code from teacher)
